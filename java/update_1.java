5bc0efe33db082ab5fc6aa5d8175cbb7749ef74c
Mr.Divyansh_Singh <120673386+divyanshsingh31422@users.noreply.github.com>
2025-12-24 03:04:27 +0530

    Freeing Memory Allocated
    Added code to demonstrate dynamic memory allocation and deallocation in C++.

--- java/update_1.java
+++ java/update_1.java
@@ -1,1 +1,13 @@
-code liku
+ Freeing Memory Allocated to
+#include <iostream>
+using namespace std;
+int main() {
+    // Declare an array of two pointers to int
+    int *p[2];
+    // Dynamically allocate memory for the integers
+    p[0] = new int(10);  // Allocate memory and initialize with 10
+    p[1] = new int(20);  // Allocate memory and initialize with 20
+    cout << "Value of *p[0]: " << *p[0] << endl;  // Outputs 10
+    cout << "Value of *p[1]: " << *p[1] << endl;  // Outputs 20
+    // Free the memory allocated for each pointer
+    delete p[0];
