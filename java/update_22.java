6ab295162f9389511b114e43ff4275eecf6915a9
Mr.Divyansh_Singh <120673386+divyanshsingh31422@users.noreply.github.com>
2026-01-03 03:15:50 +0530

    Add abstract Shape class and Circle implementation

--- java/update_22.java
+++ java/update_22.java
@@ -1,1 +1,9 @@
-update 22 
+java
+abstract class Shape {
+    abstract void draw();  // Pure virtual method
+}
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
