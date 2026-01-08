d5e6f690b1cd690413da6261cd3a29bf7b8a3582
Mr.Divyansh_Singh <120673386+divyanshsingh31422@users.noreply.github.com>
2026-01-08 05:34:35 +0530

    Implement solution for missing numbers in array
    Added a solution to find all numbers that are missing from an array.

--- java/update_28.java
+++ java/update_28.java
@@ -1,1 +1,47 @@
-update 28 
+
+Code
+Testcase
+Testcase
+Test Result
+Q3. Find All Numbers Disappeared in an Array
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+
+
+  class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        // Mark visited numbers
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        // Find missing numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
