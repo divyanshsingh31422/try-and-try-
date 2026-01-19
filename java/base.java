eb35674d85487c1f8ec12b2821545481a296d1a7
Divyansh_Singh <120673386+divyanshsingh31422@users.noreply.github.com>
2026-01-19 18:49:15 +0530

    Implement finalPrices method in Solution class

--- java/base.java
+++ java/base.java
@@ -1,1 +1,18 @@
-// Java base file 
+class Solution:
+    def finalPrices(self, prices):
+        n = len(prices)
+        ans = [0] * n
+        stack = []
+
+        for i in range(n - 1, -1, -1):
+            while stack and stack[-1] > prices[i]:
+                stack.pop()
+
+            if stack:
+                ans[i] = prices[i] - stack[-1]
+            else:
+                ans[i] = prices[i]
+
+            stack.append(prices[i])
+
+        return ans
