ede7f1033ad8aff84efe1413cd1523284b32057e
Divyansh_Singh <120673386+divyanshsingh31422@users.noreply.github.com>
2026-01-25 08:09:12 +0530

    Convert update_15.py to C++ implementation

--- python/update_15.py
+++ python/update_15.py
@@ -1,1 +1,22 @@
-update 15 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        string S;
+        cin >> N >> S;
+
+        for (int i = 0; i < N; i += 2) {
+            string p = S.substr(i, 2);
+            if (p == "00") cout << 'A';
+            else if (p == "01") cout << 'T';
+            else if (p == "10") cout << 'C';
+            else cout << 'G';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
