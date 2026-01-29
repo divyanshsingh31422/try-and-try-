fbab27b26995e8167fc34db5dcd8e4d4bc4b758e
Divyansh_Singh <120673386+divyanshsingh31422@users.noreply.github.com>
2026-01-29 22:33:18 +0530

    Add fizzBuzz function to handle FizzBuzz logic

--- python/update_22.py
+++ python/update_22.py
@@ -1,1 +1,16 @@
-update 22 
+def fizzBuzz(number):
+    # 1. Check for both 3 and 5 first (or 15)
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    
+    # 2. Check for 3
+    elif number % 3 == 0:
+        print("Fizz")
+        
+    # 3. Check for 5
+    elif number % 5 == 0:
+        print("Buzz")
+        
+    # 4. Otherwise, print the number
+    else:
+        print(number)
